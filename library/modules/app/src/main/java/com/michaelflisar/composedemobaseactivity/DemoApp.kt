344953c8d2c976fd2937ff4d24aed6fbd5a8cce9
Michael Flisar <Michael.Flisar@knapp.com>
2024-09-19 10:04:30 +0200

    made App open

--- library/modules/app/src/main/java/com/michaelflisar/composedemobaseactivity/DemoApp.kt
+++ library/modules/app/src/main/java/com/michaelflisar/composedemobaseactivity/DemoApp.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.michaelflisar.composethemer.ComposeTheme
 import com.michaelflisar.composethemer.themes.ComposeThemes
 
-class DemoApp : Application() {
+open class DemoApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
