75edb341605ab88ae3648bd4e1f47b444259303a
Michael Flisar <Michael.Flisar@knapp.com>
2024-12-04 08:08:11 +0100

    ...

--- library/modules/ui/src/commonMain/kotlin/com/michaelflisar/toolbox/composables/MyPicker.kt
+++ library/modules/ui/src/commonMain/kotlin/com/michaelflisar/toolbox/composables/MyPicker.kt
@@ -2,15 +2,29 @@ package com.michaelflisar.toolbox.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.michaelflisar.toolbox.ToolboxDefaults
 
 @Composable
-fun MyInputPicker(
+fun MyPicker(
+    value: String,
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    maxLines: Int = 1,
+    enabled: Boolean = true,
+    onStartPicker: () -> Unit
+) {
+    MyPicker(value, { Icon(icon, null) }, modifier, maxLines, enabled, onStartPicker)
+}
+
+@Composable
+fun MyPicker(
     value: String,
     icon: @Composable () -> Unit,
     modifier: Modifier = Modifier,
