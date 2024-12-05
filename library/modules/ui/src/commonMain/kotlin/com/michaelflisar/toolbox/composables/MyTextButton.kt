65625670dbe716ad1ac9e8648e91330b8662bf6f
Michael Flisar <Michael.Flisar@knapp.com>
2024-12-05 08:18:25 +0100

    MyTextButton + DesktopConfirmDialog added

---
+++ library/modules/ui/src/commonMain/kotlin/com/michaelflisar/toolbox/composables/MyTextButton.kt
@@ -0,0 +1,47 @@
+package com.michaelflisar.toolbox.composables
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MyTextButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    iconTint: Color? = null,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier,
+        colors = colors,
+        enabled = enabled,
+        onClick = onClick
+    ) {
+        Text(text)
+    }
+}
+
+@Composable
+fun MyTextOutlinedButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier,
+        colors = colors,
+        enabled = enabled,
+        onClick = onClick
+    ) {
+        Text(text)
+    }
+}
