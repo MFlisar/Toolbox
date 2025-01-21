19bfa3fedf70be707d6e87e3ca4a7f8b7c106c1a
Michael Flisar <Michael.Flisar@knapp.com>
2025-01-21 14:11:59 +0100

    default theme changed

--- library/modules/android-demo-app/src/main/java/com/michaelflisar/toolbox/androiddemoapp/classes/DemoPrefs.kt
+++ library/modules/android-demo-app/src/main/java/com/michaelflisar/toolbox/androiddemoapp/classes/DemoPrefs.kt
@@ -1,6 +1,7 @@
 package com.michaelflisar.toolbox.androiddemoapp.classes
 
 import com.michaelflisar.composethemer.ComposeTheme
+import com.michaelflisar.composethemer.themes.themes.ThemeDefault
 import com.michaelflisar.composethemer.themes.themes.ThemeGreenForest
 import com.michaelflisar.kotpreferences.core.SettingsModel
 import com.michaelflisar.kotpreferences.core.enumPref
@@ -8,7 +9,7 @@ import com.michaelflisar.kotpreferences.storage.datastore.DataStoreStorage
 import com.michaelflisar.kotpreferences.storage.datastore.create
 
 object DemoPrefs : SettingsModel(DataStoreStorage.create(name = "demo_prefs")) {
-    val themeKey by stringPref(ThemeGreenForest.KEY)
+    val themeKey by stringPref(ThemeDefault.KEY)
     val baseTheme by enumPref(ComposeTheme.BaseTheme.System)
     val dynamic by boolPref(false)
 }
