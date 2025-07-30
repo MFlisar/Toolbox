e2e0fa8ae8a56da04ebf7996e948bf44a2186476
MFlisar <mflisar.development@gmail.com>
2025-07-30 20:41:30 +0200

    ...

---
+++ library/app/src/featureNoBackupSupportMain/kotlin/com/michaelflisar/toolbox/app/features/backup/PreferencesBackup.featureNoBackupSupport.kt
@@ -0,0 +1,9 @@
+package com.michaelflisar.toolbox.app.features.backup
+
+@androidx.compose.runtime.Composable
+internal actual fun com.michaelflisar.composepreferences.core.scopes.PreferenceGroupScope.PreferencesBackup(
+    backupSupport: IBackupSupport,
+    appName: String
+) {
+    // --
+}
