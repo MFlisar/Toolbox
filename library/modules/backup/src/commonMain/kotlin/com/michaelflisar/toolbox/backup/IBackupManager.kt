4a313f675aa8e72b71665af9357f26123d8d5230
MFlisar <mflisar.development@gmail.com>
2025-07-30 14:04:00 +0200

    ...

--- library/modules/backup/src/commonMain/kotlin/com/michaelflisar/toolbox/backup/IBackupManager.kt
+++ library/modules/backup/src/commonMain/kotlin/com/michaelflisar/toolbox/backup/IBackupManager.kt
@@ -8,6 +8,11 @@ expect class ActivityNotFoundException : RuntimeException
 expect class ZipFileContentFile
 expect sealed class ZipFileContent: IZipContent<ZipFileContentFile>
 expect object BackupManager : IBackupManager<ZipFileContentFile, ZipFileContent>
+{
+    override fun onBackupRestored()
+    override suspend fun backup(files: List<ZipFileContent>, backupFile: PlatformFile): Throwable?
+    override suspend fun restore(files: List<ZipFileContent>, backupFile: PlatformFile): Throwable?
+}
 
 interface IBackupManager<ZipFileContentFile, ZipFileContent: IZipContent<ZipFileContentFile>> {
 
